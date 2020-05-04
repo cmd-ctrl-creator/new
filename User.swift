c58b9a7c15888df830a77998765cc9168009964f
George Praneeth <33452599+cmd-ctrl-creator@users.noreply.github.com>
2020-05-04 23:18:37 +0530

    Add files via upload

---
+++ User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  TableviewDiifableDS
+//
+//  Created by N GEORGE PRANEETH on 01/04/20.
+//  Copyright © 2020 N GEORGE PRANEETH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct User: Hashable {
+   var name: String
+}
